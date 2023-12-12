c3bb7b1dae521d6e9b8f37bbdddd5bc4e90b0923
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-12-12 12:59:37 +0100

    betty

---
+++ betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a beautiful code that passes betty checks
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Hello, Betty!\n");
+	return (0);
+}
