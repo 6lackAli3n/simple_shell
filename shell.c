de2516d3395a761f4b7f3c4a8b62a4cb1f5445b0
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-12-14 19:46:12 +0100

    args

--- shell.c
+++ shell.c
@@ -5,12 +5,15 @@
 #include <sys/wait.h>
 #include <string.h>
 
+
+void execute_command(char *buffer);
+
 /**
- * main - Entry point for the simple shell
+ * handle_user_input - Handle user input,
+ * prompt for commands, and execute them
  *
- * Return: Always 0 on success
  */
-int main(void)
+void handle_user_input(void)
 {
 	char *buffer;
 	size_t bufsize = 32;
@@ -32,19 +35,48 @@ int main(void)
 		}
 		if (fork() == 0)
 		{
-			buffer[strlen(buffer) - 1] = '\0';
-			if (execlp(buffer, buffer, NULL) == -1)
-			{
-				perror(buffer);
-			}
-			exit(0);
+			execute_command(buffer);
 		}
 		else
 		{
 			wait(NULL);
 		}
 	}
-
 	free(buffer);
+}
+/**
+ * execute_command - Execute a command with its arguments
+ *
+ * @buffer: The command line input to be executed
+ */
+void execute_command(char *buffer)
+{
+	char *args[32];
+	int i;
+
+	char *token = strtok(buffer, " ");
+
+	buffer[strlen(buffer) - 1] = '\0';
+
+	for (i = 0;  token != NULL; i++)
+	{
+		args[i] = token;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+	if (execvp(args[0], args) == -1)
+	{
+		perror(args[0]);
+	}
+	exit(0);
+}
+/**
+ * main - Entry point for the simple shell
+ *
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+	handle_user_input();
 	return (0);
 }
