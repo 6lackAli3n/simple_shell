08b613e3622451731c5891064b14a38d5fc108c7
6lackAli3n <abubakarabdullahayo26@gmail.com>
2023-12-15 01:06:17 +0100

    baddest

--- shell.c
+++ shell.c
@@ -1,101 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
-
-void execute_command(char *buffer);
-
-/**
- * handle_user_input - Handle user input,
- * prompt for commands, and execute them
- *
- */
-void handle_user_input(void)
-{
-	char *buffer;
-	size_t bufsize = 32;
-
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(1);
-	}
-	while (1)
-	{
-		printf("$ ");
-		getline(&buffer, &bufsize, stdin);
-
-		if (buffer[0] == '\n')
-		{
-			continue;
-		}
-		if (access(buffer, F_OK | X_OK) != 0)
-		{
-			perror(buffer);
-			continue;
-		}
-		if (fork() == 0)
-		{
-			execute_command(buffer);
-		}
-		else
-		{
-			wait(NULL);
-		}
-	}
-	free(buffer);
-}
-/**
- * execute_command - Execute a command with its arguments
- *
- * @buffer: The command line input to be executed
- */
-void execute_command(char *buffer)
-{
-	char *args[32];
-	int i;
-	char *token;
-	char *path;
-	char *path_token;
-
-	token = strtok(buffer, " ");
-
-	buffer[strlen(buffer) - 1] = '\0';
-
-	path = getenv("PATH");
-	path_token = strtok(path, ":");
-
-	for (i = 0;  token != NULL; i++)
-	{
-		args[i] = token;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
-
-	while (path_token != NULL)
-	{
-		char *full_path = malloc(strlen(path_token) + strlen(args[0]) + 2);
-
-		sprintf(full_path, "%s/%s", path_token, args[0]);
-		if (access(full_path, X_OK) == 0)
-		{
-			if (execve(full_path, args, NULL) == -1)
-			{
-				perror(args[0]);
-			}
-			free(full_path);
-			exit(0);
-		}
-		free(full_path);
-		path_token = strtok(NULL, ":");
-	}
-	perror(args[0]);
-	exit(127);
-}
 /**
  * main - Entry point for the simple shell
  *
@@ -103,6 +12,43 @@ void execute_command(char *buffer)
  */
 int main(void)
 {
-	handle_user_input();
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t characters;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		characters = getline(&buffer, &bufsize, stdin);
+		if (characters == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
+		}
+		buffer[strcspn(buffer, "\n")] = '\0';
+
+		if (fork() == 0)
+		{
+			char **args = malloc(2 * sizeof(char *));
+
+			if (args == NULL)
+			{
+				perror("malloc");
+				exit(EXIT_FAILURE);
+			}
+			args[0] = buffer;
+			args[1] = NULL;
+
+			execve(buffer, args, NULL);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	free(buffer);
 	return (0);
 }
